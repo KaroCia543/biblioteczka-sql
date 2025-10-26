09f903286ebabdd3122f8ff48013b207884466e5
KaroCia543 <k.ciasto@interia.pl>
2025-10-26 19:57:25 +0100

    Manipulacja danymi - zadanie 2CD

---
+++ pracownicy2.sql
@@ -0,0 +1,73 @@
+-- 1,2,3
+TRUNCATE TABLE kursanci;
+
+SELECT * FROM pracownicy;
+
+ALTER TABLE pracownicy
+RENAME COLUMN NR TO ID;
+
+TRUNCATE TABLE pracownicy;
+
+SELECT * FROM pracownicy;
+
+ALTER TABLE pracownicy
+MODIFY COLUMN Imię VARCHAR(100) NULL,
+MODIFY COLUMN Nazwisko VARCHAR(100) NULL;
+
+INSERT INTO pracownicy (ID, Imię, Nazwisko, Wiek, Kurs) 
+VALUES
+(1, 'Anna', 'NOWAK', 34, 'DS.'),
+(2, 'Roman', 'KOWALSKI', 42, 'DS.'),
+(3, 'Tomasz', 'WIŚNIEWSKI', 33, 'DS.'),
+(4, 'Agata', 'WÓJCIK', 43, 'DS.'),
+(5, 'Elżbieta', 'KOWALCZYK', 28, 'Java'),
+(6, 'Przemysław', NULL, 34, 'Java'),
+(7, 'Robert', NULL, 35, 'Java'),
+(8, 'Radosław', 'ZIELIŃSKI', 38, 'Java'),
+(9, NULL, 'WOŹNIAK', 26, 'Java'),
+(10, 'Robert', 'SZYMAŃSKI', 34, 'Java'),
+(11, 'Radosław', 'DĄBROWSKI', 35, NULL),
+(12, 'Robert', 'KOZŁOWSKI', NULL, 'UX'),
+(13, 'Joanna', 'MAZUR', 26, 'UX'),
+(14, 'Radosław', 'JANKOWSKI', 27, 'UX'),
+(15, 'Patryk', 'LEWANDOWSKI', 28, 'Tester'),
+(16, NULL, 'ZIELIŃSKI', 28, 'Tester'),
+(17, 'Andrzej', 'WOŹNIAK', 31, 'Tester'),
+(18, 'Andrze', 'LEWANDOWSKI', 30, 'Tester'),
+(19, 'Roman', NULL, 39, 'Tester'),
+(20, 'Ewa', 'WOŹNIAK', 31, 'Tester');
+
+SELECT * FROM pracownicy;
+
+-- 4
+SELECT * 
+FROM pracownicy
+WHERE wiek LIKE 28;
+
+-- 5
+SELECT *
+FROM pracownicy
+WHERE wiek <= 30;
+
+-- 6
+SELECT * 
+FROM pracownicy 
+WHERE nazwisko LIKE "%ski%";
+
+-- 7
+SELECT *
+FROM pracownicy
+WHERE ID IN (1, 4, 7, 18, 20);
+
+-- 8
+SELECT * 
+FROM pracownicy
+WHERE Imię IS NOT NULL
+AND Nazwisko IS NOT NULL
+AND wiek IS NOT NULL
+AND kurs IS NOT NULL;
+
+-- 9
+SELECT * 
+FROM pracownicy 
+WHERE kurs != "DS.";
